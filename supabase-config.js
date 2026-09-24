4f12d35476aec6b1a398582bbadf6c1ad4d261ea
abrahammendozaalmeida-boop <abrahammendozaalmeida@gmail.com>
2026-09-24 00:07:33 -0600

    Configurar conexión pública de Supabase

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
-window.MARVEL_HUB_SUPABASE_URL = "";
-window.MARVEL_HUB_SUPABASE_PUBLISHABLE_KEY = "";
+window.MARVEL_HUB_SUPABASE_URL = "https://yovwrdhopuooyywwzlzp.supabase.co";
+window.MARVEL_HUB_SUPABASE_PUBLISHABLE_KEY = "sb_publishable_vKd98jmFNLVmjNnmbxDnBA_UfmzHbSA";
